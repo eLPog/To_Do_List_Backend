5a4a997c7248dd62908239be31f50c856c4516b7
LukasP <lukas.straubing@gmail.com>
2022-06-17 21:38:48 +0200

    added config example

---
+++ config/config.ts
@@ -0,0 +1,11 @@
+export const configExample = {
+    dbHost: 'localhost',
+    user:'root',
+    password:'',
+    database:'to_do_list_version1',
+    jwtAccess:'109j3roefid0--203oeidf0-1-03uofrdkpdcnvicwoq][pnfpkvn9203kbf23fnoid02on.nvucwnomp428hbiq=-0rgbv2nvdkwnvo',
+    emailHost:'emailHost',
+    emailPort:'emailPort',
+    emailUser:'emailUser',
+    emailPass:'emailPass',
+}
