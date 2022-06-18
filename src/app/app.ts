6b30b18b38d5c8bd6b5281ef30be36faf0c97f90
LukasP <lukas.straubing@gmail.com>
2022-06-18 13:37:13 +0200

    cros origin address added

--- src/app/app.ts
+++ src/app/app.ts
@@ -47,9 +47,9 @@ class App {
     }
 
     private routes() {
-        this.app.use('/api/v1/auth', authRouter)
-        this.app.use('/api/v1/user', isAuth, userRouter)
-        this.app.use('/api/v1/tasks', isAuth, taskRouter)
+        this.app.use('/api/auth', authRouter)
+        this.app.use('/api/user', isAuth, userRouter)
+        this.app.use('/api/tasks', isAuth, taskRouter)
     }
     private errorHandlers(){
         this.app.use('/', notFoundHandler)
